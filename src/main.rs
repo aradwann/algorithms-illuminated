e0f0278f5587a18326e4be77db666d7ea971bbb7
Ahmed Radwan <aradwann@proton.me>
2023-01-05 20:35:06 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use algorithms_illuminated::sort::merge;
+
+fn main() {
+    merge(vec![1, 2, 3, 4]);
+    println!("hello from bin");
+}
---
+++ src/sort/merge.rs
@@ -0,0 +1,14 @@
+///
+pub fn merge(arr: Vec<i32>) -> Vec<i32> {
+    arr
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge_sort() {
+        assert_eq!(3, 4);
+    }
+}
