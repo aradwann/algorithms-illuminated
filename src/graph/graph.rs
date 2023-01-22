d27e8341c20ce37f497ee983481e33d6cc430460
Ahmed Radwan <aradwann@proton.me>
2023-01-22 16:55:43 +0200

    impl draft bfs

--- src/graph/graph.rs
+++ src/graph/graph.rs
@@ -1,4 +1,6 @@
-/// representing a graph using adjancy list which is
+use std::collections::VecDeque;
+
+/// representing a graph using an adjacency list which is
 /// 1) An array containing the graph nodes
 /// 2) An array containing the graph edges
 /// 3) For each edge, a pointer to each of its two endpoints
@@ -6,9 +8,11 @@
 
 type NodeIndex = usize;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct NodeData {
     edges: Vec<EdgeIndex>,
+    value: char,
+    explored: bool,
 }
 
 type EdgeIndex = usize;
@@ -17,6 +21,7 @@ type EdgeIndex = usize;
 struct UndirectedEdgeData(NodeIndex, NodeIndex);
 impl UndirectedEdgeData {
     fn new(node_1: NodeIndex, node_2: NodeIndex) -> Self {
+        // order of nodes does not matter as it is undirected
         if node_1 > node_2 {
             Self(node_2, node_1)
         } else {
@@ -36,9 +41,13 @@ impl UndirectedGraph {
             edges: vec![],
         }
     }
-    pub fn add_node(&mut self) -> NodeIndex {
+    pub fn add_node(&mut self, value: char) -> NodeIndex {
         let index = self.nodes.len();
-        self.nodes.push(NodeData { edges: vec![] });
+        self.nodes.push(NodeData {
+            edges: vec![],
+            value,
+            explored: false,
+        });
         index
     }
     pub fn add_edge(&mut self, source: NodeIndex, target: NodeIndex) -> EdgeIndex {
@@ -51,5 +60,39 @@ impl UndirectedGraph {
         let node_data_2 = &mut self.nodes[target];
         node_data_2.edges.push(edge_index);
         edge_index
+    }
+
+    /// Pseudocode
+    /// Input: graph G=(V,E) in adjancency list representation and a vertex s ∈ V
+    /// postcondition: a vertex is reachabele from s if and only f it is marked as explored
+    /// -----------------------------------------------------------------------------------
+    /// mark s as explored, all other vertices as unexplored
+    /// Q := a queue data structure, intialized with s
+    /// while Q is not empty do
+    ///     remove the vertex from the front of the Q, call it v
+    ///     for each edge (v,w) in v's adjacency list do
+    ///         if w is unexplored then
+    ///         mark w as explored
+    ///         add w to the end of Q
+    pub fn bfs(&mut self, _value: char) {
+        self.mark_all_nodes_unexplored();
+        let mut queue = VecDeque::new();
+        queue.push_back(self.edges[0].0); // push the first node data into the queue
+        while queue.len() != 0 {
+            let node_index = queue.pop_front().unwrap(); // the index of the node
+            let node_data = self.nodes[node_index].clone();
+            for edge_index in node_data.edges {
+                let UndirectedEdgeData(_, node_index_2) = self.edges[edge_index];
+                let node_2 = &mut self.nodes[node_index_2];
+                if node_2.explored == false {
+                    node_2.explored = true;
+                    println!("exploring Node {:#?}", &node_2);
+                    queue.push_back(node_index_2);
+                }
+            }
+        }
+    }
+    fn mark_all_nodes_unexplored(&mut self) {
+        self.nodes.iter_mut().map(|n| n.explored = false);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,23 @@ use algorithms_illuminated::graph::graph::UndirectedGraph;
 
 fn main() {
     let mut graph = UndirectedGraph::new();
-    println!("{:?}", graph);
-    graph.add_node();
-    graph.add_node();
+    println!("{:#?}", graph);
+    graph.add_node('s');
+    graph.add_node('a');
+    graph.add_node('b');
+    graph.add_node('c');
+    graph.add_node('d');
+    graph.add_node('e');
 
     graph.add_edge(0, 1);
-    println!("{:?}", graph);
+    graph.add_edge(0, 2);
+    graph.add_edge(1, 3);
+    graph.add_edge(2, 3);
+    graph.add_edge(2, 4);
+    graph.add_edge(3, 4);
+    graph.add_edge(3, 5);
+    graph.add_edge(4, 5);
+    println!("{:#?}", graph);
+
+    graph.bfs('k');
 }
